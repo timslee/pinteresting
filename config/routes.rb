42de176153c3345107ef37f63651a6f10b96b7cd
Tim Lee <timslee@gmail.com>
2015-04-01 02:02:36 -0700

    Creating home, about and contact us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get "about" => 'pages#about'
+  get "contactus" => 'pages#contactus'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
